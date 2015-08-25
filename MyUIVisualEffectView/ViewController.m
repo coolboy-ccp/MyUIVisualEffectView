7a8e68e617c0809e7b078b6e09eac7e985a45280
brian-chu <chuchengpeng@liqunfeideMac-mini.local>
2015-08-25 15:13:32 +0800

    毛玻璃

--- MyUIVisualEffectView/ViewController.m
+++ MyUIVisualEffectView/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIVisualEffectView *visualEfView;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *mainImageView;
 
 @end
 
@@ -16,6 +20,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.mainImageView.image = [UIImage imageNamed:@"1.jpg"];
+    visualEfView = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleDark]];
+    visualEfView.frame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height/2);
+    visualEfView.alpha = 0.9;
+    [self.mainImageView addSubview:visualEfView];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
