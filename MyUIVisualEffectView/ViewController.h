0b1a94eb41c24f074c1b4b813dff9d76c44e8d03
brian-chu <chuchengpeng@liqunfeideMac-mini.local>
2015-08-25 14:57:56 +0800

    Initial Commit

---
+++ MyUIVisualEffectView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyUIVisualEffectView
+//
+//  Created by liqunfei on 15/8/25.
+//  Copyright (c) 2015年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
